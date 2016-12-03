d9af3b63ed97fa4e2140a2bc8bbd51c7d6fd9ce0
john nicholas <themrtortoise@gmail.com>
2016-12-03 00:14:32 +0000

    akka to akka using io works ... using a socket gets me a protocol problem.

---
+++ Akka.IO.Samples/Akka.IO.ConsoleTextClient/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Akka.IO.ConsoleTextClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 34000);
+
+            Socket sender = new Socket(AddressFamily.InterNetwork,
+               SocketType.Stream, ProtocolType.Unspecified);
+
+            sender.Connect(remoteEP);
+
+            Console.WriteLine("Socket connected to {0}",
+                sender.RemoteEndPoint.ToString());
+
+            while(true)
+            {
+                var input = Console.ReadLine();
+                var encodedInput = Encoding.UTF8.GetBytes(input);
+
+                int bytesSent = sender.Send(encodedInput);
+                var inputString = new ArraySegment<byte>(encodedInput, 0, encodedInput.Length);  
+   
+          
+                //var receiveTasl = client.ReceiveAsync(received,new CancellationToken());
+
+                //receiveTasl.ContinueWith(res =>
+                //{
+                //    if (res.IsCompleted)
+                //    {
+                //        Console.WriteLine(Encoding.UTF8.GetChars(received.Array));
+                //    }
+                    
+
+                //    if (res.IsFaulted)
+                //    {
+                //        Console.WriteLine("Bang");
+                //        Console.WriteLine(res.Exception.Flatten().ToString());
+                //    }
+                //});
+            }
+            
+
+        }
+    }
+}
---
+++ Akka.IO.Samples/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.IO;
+using Akka.Actor;
+using System.Net;
+
+namespace Akka.IO.Samples
+{
+    class Program
+    {
+        public static IActorRef server;
+
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("example");
+            var manager = system.Tcp();
+
+            server = system.ActorOf(Props.Create<EchoServer>(() => new EchoServer(34000)));
+
+            Console.ReadLine();
+        }
+    }
+
+    class EchoServer : UntypedActor
+    {
+        public EchoServer(int port)
+        {
+            Context.System.Tcp().Tell(new Tcp.Bind(Self, new IPEndPoint(IPAddress.Any, port)));
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is Tcp.Bound)
+            {
+                var bound = message as Tcp.Bound;
+                Console.WriteLine("Listening on {0}", bound.LocalAddress);
+            }
+            else if (message is Tcp.Connected)
+            {
+                var connection = Context.ActorOf(Props.Create(() => new EchoConnection(Sender)));
+                Sender.Tell(new Tcp.Register(connection));
+            }
+            else Unhandled(message);
+        }
+    }
+
+    class EchoConnection : UntypedActor
+    {
+        private readonly IActorRef _connection;
+
+        public EchoConnection(IActorRef connection)
+        {
+            _connection = connection;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            if (message is Tcp.Received)
+            {
+                var received = message as Tcp.Received;
+                if (received.Data.Head == 'x')
+                    Context.Stop(Self);
+                else
+                {
+                    Console.WriteLine(Encoding.ASCII.GetString(received.Data.ToArray()));
+                    _connection.Tell(Tcp.Write.Create(received.Data));
+                }
+            }
+            else Unhandled(message);
+        }
+    }
+}
